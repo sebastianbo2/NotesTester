0ca86efc3d23459e449685b242dcd0eda908be2e
firedespire <7sebastianbo11@gmail.com>
2026-01-12 18:16:10 -0500

    Update index.js backend

--- backend/index.js
+++ backend/index.js
@@ -144,6 +144,7 @@ app.post("/api/answers", express.json(), async (req, res) => {
 
 export default app;
 
+
 // app.listen(process.env.PORT || 8000, () => {
 //   console.log("Server is listening");
 // });
